0312f31fa91db8567a689c11534f7e5971061164
afshar-dev <99553574+thepermonica@users.noreply.github.com>
2023-07-04 11:38:41 +0330

    Refactor

--- app/Http/Controllers/Site/Home/HomeController.php
+++ app/Http/Controllers/Site/Home/HomeController.php
@@ -5,10 +5,8 @@ namespace App\Http\Controllers\Site\Home;
 use App\Http\Controllers\Controller;
 use App\Repositories\Site\HomeRepository;
 use Artesaos\SEOTools\Facades\JsonLd;
-use Artesaos\SEOTools\Facades\JsonLdMulti;
 use Artesaos\SEOTools\Facades\OpenGraph;
 use Artesaos\SEOTools\Facades\SEOMeta;
-use Artesaos\SEOTools\Facades\TwitterCard;
 
 class HomeController extends Controller
 {
@@ -22,26 +20,18 @@ class HomeController extends Controller
 
     public function homeIndex()
     {
-        SEOMeta::setTitle('مبدل گر - خواهش میکنم - فلان و هر چیزی که به ذهنت میاد');
-        SEOMeta::setDescription('This is my page description');
-        SEOMeta::setCanonical('https://codecasts.com.br/lesson');
+        SEOMeta::setTitle('در حال راه اندازی');
+        SEOMeta::setDescription('در حال راه اندازی');
+        SEOMeta::setCanonical(url('/'));
 
-        OpenGraph::setDescription('This is my page description');
-        OpenGraph::setTitle('Home');
-        OpenGraph::setUrl('http://current.url.com');
+        OpenGraph::setDescription('در حال راه اندازی');
+        OpenGraph::setTitle('در حال راه اندازی');
+        OpenGraph::setUrl(url('/'));
         OpenGraph::addProperty('type', 'articles');
 
-        TwitterCard::setTitle('Homepage');
-        TwitterCard::setSite('@LuizVinicius73');
-
-        JsonLd::setTitle('Homepage');
-        JsonLd::setDescription('This is my page description');
-        JsonLd::addImage('https://codecasts.com.br/img/logo.jpg');
-
-
-        JsonLdMulti::setTitle("title");
-        JsonLdMulti::setDescription("description");
-        JsonLdMulti::setType('Article');
+        JsonLd::setTitle('در حال راه اندازی');
+        JsonLd::setDescription('در حال راه اندازی');
+//        JsonLd::addImage('https://codecasts.com.br/img/logo.jpg');
 
         return view('site.home.index');
     }
