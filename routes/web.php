86ad6181dd9dc04660d9bd7e10714d88d544fc20
afshar-dev <99553574+thepermonica@users.noreply.github.com>
2023-07-22 18:51:18 +0330

    add Sitemap

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,42 @@
 <?php
 
+use App\Models\Category\Category;
 use Illuminate\Support\Facades\Route;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+
+Route::get('/sitemap', function () {
+    $path = public_path('sitemap.xml');
+    $categories = Category::query()
+        ->select([
+            'id',
+            'slug',
+            'title',
+            'updated_at'
+        ])
+        ->where('is_active', 1)
+        ->where('parent_id', 0)
+        ->with(['images', 'children'])
+        ->get();
+    $sitemap = Sitemap::create();
+    foreach ($categories as $category) {
+        $sitemap->add(Url::create(route('site.categories.show', $category->slug))
+            ->setLastModificationDate($category->updated_at)
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+            ->setPriority(1.0)
+            ->addImage($category->images->url, $category->title)
+        );
+        foreach ($category->children as $children) {
+            $sitemap->add(Url::create(route('site.categories.child', [$category->slug, $children->slug]))
+                ->setLastModificationDate($children->updated_at)
+                ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+                ->setPriority(1.0)
+                ->addImage($children->images->url, $children->title)
+            );
+        }
+    }
+    $sitemap->writeToFile($path);
+})->middleware(['web', 'auth', 'super.admin']);
 
 Route::get('/script-test', function () {
     return view('site.categories.script-test');
