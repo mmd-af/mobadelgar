9c489fc93fb14a6a8556d0db1cbf040cf0550dc3
afshar.dev <thepermonica@gmail.com>
2024-03-23 09:09:56 +0200

    Refactor

--- resources/views/site/categories/script-test.blade.php
+++ resources/views/site/categories/script-test.blade.php
@@ -15,26 +15,55 @@
         <div class="row justify-content-center" id="root">
             <div class="row justify-content-center mt-5">
 
-      <div id="dtp1"></div>
+
+                <div class="col-md-6">
+                    <h3>Miladi to Shamsi</h3>
+                    <div class="input-group mb-3">
+                        <input type="text" id="miladiInput" class="form-control" placeholder="YYYY-MM-DD">
+                        <button class="btn btn-primary" onclick="convertToShamsi()">Convert</button>
+                    </div>
+                    <p id="shamsiResult"></p>
+                </div>
+                <div class="col-md-6">
+                    <h3>Shamsi to Miladi</h3>
+                    <div class="input-group mb-3">
+                        <input type="text" id="shamsiInput" class="form-control" placeholder="YYYY/MM/DD">
+                        <button class="btn btn-primary" onclick="convertToMiladi()">Convert</button>
+                    </div>
+                    <p id="miladiResult"></p>
+                </div>
 
             </div>
         </div>
-    </div>
-    <div class="container-fluid">
-        <hr class="bg-secondary py-5">
-    </div>
-    <div class="container bg-white rounded-3 p-5">
-
     </div>
 @endsection
 
 @section('script')
     <script>
 
-        const dtp1Instance = new mds.MdsPersianDateTimePicker(document.getElementById('dtp1'), {
-            targetTextSelector: '[data-name="dtp1-text"]',
-            targetDateSelector: '[data-name="dtp1-date"]',
+        const persianDatepickerMiladi = new PersianDatepicker('#miladiInput', {
+            format: 'YYYY-MM-DD',
+            observer: true,
+            autoClose: true
         });
+
+        const persianDatepickerShamsi = new PersianDatepicker('#shamsiInput', {
+            format: 'YYYY/MM/DD',
+            observer: true,
+            autoClose: true
+        });
+
+        function convertToShamsi() {
+            const miladiInput = document.getElementById('miladiInput').value;
+            const shamsiDate = moment(miladiInput, 'YYYY-MM-DD').format('jYYYY/jM/jD');
+            document.getElementById('shamsiResult').innerText = "Shamsi Date: " + shamsiDate;
+        }
+
+        function convertToMiladi() {
+            const shamsiInput = document.getElementById('shamsiInput').value;
+            const miladiDate = moment(shamsiInput, 'jYYYY/jM/jD').format('YYYY-MM-DD');
+            document.getElementById('miladiResult').innerText = "Miladi Date: " + miladiDate;
+        }
 
     </script>
 @endsection
